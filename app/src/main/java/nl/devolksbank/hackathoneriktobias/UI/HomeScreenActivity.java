1a4727b820531b3393cca50540045a781cf8c957
Erik Brandsma <erik.brandsma@devolksbank.nl>
2018-05-17 15:01:37 +0200

    HomeScreen

---
+++ app/src/main/java/nl/devolksbank/hackathoneriktobias/UI/HomeScreenActivity.java
@@ -0,0 +1,14 @@
+package nl.devolksbank.hackathoneriktobias.UI;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import nl.devolksbank.hackathoneriktobias.R;
+
+public class HomeScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_screen);
+    }
+}
